245a7adf58d9ed52edc1d6038af41ad380766401
Yao Long <yaolongt@gmail.com>
2023-10-11 10:27:54 +0800

    added delete by IDs, update by ID

--- crud_functions.py
+++ crud_functions.py
@@ -26,7 +26,8 @@ def get_all_challenges():
 
 
 def get_challenge_by_ids(data={}):
-    """Get challenges by the IDs provided.
+    """Get challenges by the IDs provided;
+        `_id` must be provided with a List value.
 
     Args:
         data (dict, optional): A dict object that consists of a key of '_id' and a list of IDs. Defaults to {}.
@@ -59,7 +60,8 @@ def get_challenge_by_ids(data={}):
 
 
 def add_challenge(data={}):
-    """ Add a challenge into the database
+    """ Add a challenge into the database;
+        Only 1 request body is required.
 
     Returns:
         JSON: JSON object of the response result
@@ -85,8 +87,81 @@ def add_challenge(data={}):
         except errors.DuplicateKeyError as e:
             return Response("Bad Request", f"Image URL {challenge['image_url']} is duplicated!").to_dict()
 
-if __name__ == '__main__':
-    for data in test.create_users():
-        print(f'{add_challenge(data)}\n')
-    print(get_all_challenges())
-    print(get_challenge_by_ids({"_id": [ "6524bec1adb7238a3c812a8b" ]}))
+
+def update_challenge_by_id(data={}):
+    """Update a challenge by ID;
+        Only 1 request body is required.
+
+    Args:
+        id (string): A string that represents the ObjectID of the challenge. Defaults to empty string "".
+
+    Returns:
+        JSON: JSON object of the response result
+    """
+    if not data:
+        return Response("Bad Request", "Request body cannot be empty and a challenge ID is required").to_dict()
+    
+    if not data or not data.get('_id'):
+        return Response("Bad Request", "Request body and challenge ID is required").to_dict()
+
+    challenge_data = Challenge(**data)
+    is_valid, err = challenge_data.validate()
+
+    if not is_valid:
+        return Response("Bad Request", err).to_dict()
+    else:
+        challenge = challenge_data.to_dict()
+        id = challenge.pop("_id")
+        try:
+            challenge_collection = db_connection.get_db_collection()
+            
+            res = challenge_collection.find_one_and_update({"_id": id}, {'$set': challenge})
+            if res:
+                return Response("Success", challenge).to_dict()
+            else:
+                return Response("Not Found", "Challenge not found").to_dict()
+        except errors.DuplicateKeyError as e:
+            return Response("Bad Request", f"Image URL {challenge['image_url']} is duplicated!").to_dict()
+
+
+def delete_challenges_by_ids(data={}):
+    """Delete all the challenges by ID; 
+        `_id` must be provided with a List value.
+
+    Args:
+        data (dict, optional): A dict object that consists of a key of '_id' and a list of IDs. Defaults to {}.
+
+    Returns:
+        JSON: JSON object of the deleted records result
+    """
+    if not data:
+        return Response("Bad Request", "Request body cannot be empty and challenge ID is required").to_dict()
+    
+    ids = data.get("_id")
+
+    if not ids:
+        return Response("Bad Request", "A list of challenge IDs required").to_dict()
+    
+    challenge_collection = db_connection.get_db_collection()
+    
+    challenges = challenge_collection.find()
+    challenge_records = {}
+
+    for challenge in challenges:
+        id = challenge.pop("_id")
+        challenge_records[id] = challenge
+
+    # for challenge IDs not found in the DB
+    not_found = [id for id in ids if id not in challenge_records]
+    # for challenge IDs that is found in DB, get the data that is deleted
+    found = {id: challenge_records[id] for id in ids if id in challenge_records}
+    
+    res = challenge_collection.delete_many({"_id": {"$in": ids}})
+    if res.acknowledged:
+        if res.deleted_count == len(ids):
+            return Response("Success", found).to_dict()
+        else:
+            return Response("Not Found", {"Found": found, "Not Found": not_found}).to_dict()
+            
+    else:
+        return Response("Internal Server Error", f"Unable to delete all records").to_dict()
