2cef96fcc03ffeacada6ad67b699fc5ec8056f6b
Yao Long <yaolongt@gmail.com>
2023-10-11 10:27:54 +0800

    added delete by IDs, update by ID

--- testing/test_crud.py
+++ testing/test_crud.py
@@ -1,3 +1,5 @@
+import crud_functions
+
 def create_users():
     user1 = {
         "_id": "6524bec1adb7238a3c812a8b",
@@ -16,3 +18,10 @@ def create_users():
     }
 
     return [user1, user2]
+
+
+if __name__ == '__main__':
+    for data in create_users():
+        print(f'{crud_functions.add_challenge(data)}\n')
+    print(f'{crud_functions.get_all_challenges()}\n')
+    print(f'{crud_functions.get_challenge_by_ids({"_id": [ "6524bec1adb7238a3c812a8b" ]})}\n')
