71c66000ba74c3c55a2ac82594fc083168abdb41
Yao Long <yaolong.tan.2021@smu.edu.sg>
2023-10-20 02:23:13 +0800

    fixed singleton database client

--- src/db/db_connection.py
+++ src/db/db_connection.py
@@ -1,24 +1,20 @@
 import os, time
 import pymongo
 
-from logger import logger
+from src.logger import logger
 from dotenv import load_dotenv
 from pymongo import MongoClient, errors
-from db.DatabaseConnectionError import DatabaseConnectionError
 
 load_dotenv()
 
 mongo_uri = os.getenv("MONGOURI")
 client = MongoClient(mongo_uri)
     
-def get_db_collection():
-    """Get the Database `cs302` which has a Collection `challenge`.
+def get_collection():
+    """Get database connection
 
     Returns:
         Collection: A MongoClient collection
-
-    Raises:
-        DatabaseConnectionError: Connection error to database, and connection is not established
     """
     retry_timer = 2
     
@@ -37,8 +33,7 @@ def get_db_collection():
             logger.info("Connected to MongoDB SUCCESS!")
             return challenge_collection
         
-        except errors.ServerSelectionTimeoutError as e:
-            # raise DatabaseConnectionError(f'Error connecting to database: {str(e)}')
+        except errors.ServerSelectionTimeoutError:
             logger.info(f"Failed to connect to MongoDB... Retrying in {retry_timer} seconds")
             time.sleep(retry_timer)
             retry_timer += 2
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,12 @@
 import pytest
-from src import crud_functions
+from src.db import crud_functions
+from tests.conftest import client
+
 
 @pytest.mark.dependency()
-def test_get_all_challenges(client):
-    result = crud_functions.get_all_challenges(client)
+def test_get_all_challenges():
+    crud = crud_functions.CrudFunctions(client)
+    result = crud.get_all_challenges()
 
     assert result['message'] == 'Success'
     assert result['data'] == {
@@ -18,7 +21,8 @@ def test_get_all_challenges(client):
 @pytest.mark.dependency(depends=['test_get_all_challenges'])
 def test_get_challenge_by_ids(client):
     ids = {"_id": ["6524bec1adb7238a3c812a8b"]}
-    result = crud_functions.get_challenge_by_ids(ids, client)
+    crud = crud_functions.CrudFunctions(client)
+    result = crud.get_challenge_by_ids(ids)
 
     assert result['message'] == 'Success'
     assert result['data'] == {
@@ -32,94 +36,97 @@ def test_get_challenge_by_ids(client):
 
 @pytest.mark.dependency(depends=['test_get_all_challenges'])
 def test_add_challenge_success(client):
-    result = crud_functions.add_challenge({
-        "_id": "6524c308d74b9e961ddcdb1a", 
-        "email": "cs302@google.com",
-        "image_name": "another_challenge",
-        "image_ver": "1.0"}, client)
-    
-    assert result['message'] == "Successfully created"
-    assert result['data'] == {
-        "_id": "6524c308d74b9e961ddcdb1a",
-        "email": "cs302@google.com",
-        "image_name": "another_challenge",
-        "image_ver": "1.0"
-    }
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.add_challenge({
+         "_id": "6524c308d74b9e961ddcdb1a", 
+         "email": "cs302@google.com",
+         "image_name": "another_challenge",
+         "image_ver": "1.0"})
+
+     assert result['message'] == "Successfully created"
+     assert result['data'] == {
+         "_id": "6524c308d74b9e961ddcdb1a",
+         "email": "cs302@google.com",
+         "image_name": "another_challenge",
+         "image_ver": "1.0"
+     }
 
 
 @pytest.mark.dependency(depends=['test_get_all_challenges', 'test_add_challenge_success'])
 def test_add_challenge_duplicated_name_ver(client):
-    result = crud_functions.add_challenge({
-        "_id": "6524c308d74b9e961ddcdb1a", 
-        "email": "cs302@smu.com",
-        "image_name": "first_challenge",
-        "image_ver": "1.0"}, client)
-    
-    assert result['message'] == "Bad Request"
-    assert result['data'] == "Image name and version is duplicated!"
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.add_challenge({
+         "_id": "6524c308d74b9e961ddcdb1a", 
+         "email": "cs302@smu.com",
+         "image_name": "first_challenge",
+         "image_ver": "1.0"})
+
+     assert result['message'] == "Bad Request"
+     assert result['data'] == "Image name and version is duplicated!"
 
 
 @pytest.mark.dependency(depends=['test_get_all_challenges'])
 def test_add_challenge_fail(client):
-    result = crud_functions.add_challenge({}, client)
-    
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.add_challenge({})
 
-    assert result['message'] == 'Bad Request'
-    assert result['data'] ==  "Request body cannot be empty and challenge body is required"
+     assert result['message'] == 'Bad Request'
+     assert result['data'] ==  "Request body cannot be empty and challenge body is required"
 
 
 @pytest.mark.dependency(depends=['test_get_all_challenges', 'test_get_challenge_by_ids'])
 def test_get_challenge_by_ids_notfound(client):
-    ids = {"_id": ["123"]}
-    result = crud_functions.get_challenge_by_ids(ids, client)
+     ids = {"_id": ["123"]}
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.get_challenge_by_ids(ids)
 
-    assert result['message'] == 'Not Found'
-    assert result['data']['Not Found'] == ["123"]
+     assert result['message'] == 'Not Found'
+     assert result['data']['Not Found'] == ["123"]
 
 
 @pytest.mark.dependency(depends=["test_get_all_challenges"])
 def test_update_challenge_by_id(client):
-    body = {
-        "_id": "6524bec1adb7238a3c812a8b",
-        "email": "cs302@xmail.com",
-        "image_name": "updated_image",
-        "image_ver": "1.0"
-    }
-    
-    result = crud_functions.update_challenge_by_id(body, client)
+     body = {
+         "_id": "6524bec1adb7238a3c812a8b",
+         "email": "cs302@xmail.com",
+         "image_name": "updated_image",
+         "image_ver": "1.0"
+     }
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.update_challenge_by_id(body)
 
-    assert result['message'] == "Success"
-    assert result['data'] == {
-        "email": "cs302@xmail.com",
-        "image_name": "updated_image",
-        "image_ver": "1.0"
-    }
+     assert result['message'] == "Success"
+     assert result['data'] == {
+         "email": "cs302@xmail.com",
+         "image_name": "updated_image",
+         "image_ver": "1.0"
+     }
 
 
 @pytest.mark.dependency(depends=["test_get_all_challenges", "test_update_challenge_by_id"])
 def test_update_challenge_by_id_no_id(client):
-    body = {
-        "email": "cs302@xmail.com",
-        "image_name": "updated_image",
-        "image_ver": "1.0"
-    }
-    
-    result = crud_functions.update_challenge_by_id(body, client)
+     body = {
+         "email": "cs302@xmail.com",
+         "image_name": "updated_image",
+         "image_ver": "1.0"
+     }
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.update_challenge_by_id(body)
 
-    assert result['message'] == "Bad Request"
-    assert result['data'] == "Request body and challenge ID is required"
+     assert result['message'] == "Bad Request"
+     assert result['data'] == "Request body and challenge ID is required"
 
 
 @pytest.mark.dependency(depends=["test_get_all_challenges", "test_update_challenge_by_id", "test_update_challenge_by_id_no_id"])
 def test_update_challenge_by_id_notfound(client):
-    body = {
-        "_id": "6524c308d74b9e961ddcdb1a",
-        "email": "cs302@xmail.com",
-        "image_name": "updated_image",
-        "image_ver": "1.0"
-    }
-    
-    result = crud_functions.update_challenge_by_id(body, client)
+     body = {
+         "_id": "6524c308d74b9e961ddcdb1a",
+         "email": "cs302@xmail.com",
+         "image_name": "updated_image",
+         "image_ver": "1.0"
+     }
+     crud = crud_functions.CrudFunctions(client)
+     result = crud.update_challenge_by_id(body)
 
-    assert result['message'] == "Not Found"
-    assert result['data'] == "Challenge not found"
+     assert result['message'] == "Not Found"
+     assert result['data'] == "Challenge not found"
